12b42da6e18aba0cb1f59ac3aed8a4e0b20b9d18
nuxse <nuxse@nuxsedeMacBook-Air.local>
2018-04-17 14:38:59 +0800

    init

---
+++ lib/Nuxseme/Lib/CollectionInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author: nuxse
+ * Data: 2018/4/17
+ */
+
+namespace Collection;
+
+interface CollectionInterface extends \ArrayAccess, \Countable, \IteratorAggregate
+{
+    public function set($key, $value);
+
+    public function get($key, $default = null);
+
+    public function replace(array $items);
+
+    public function all();
+
+    public function has($key);
+
+    public function remove($key);
+
+    public function clear();
+}
